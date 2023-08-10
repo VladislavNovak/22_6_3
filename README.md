2b9479bdf5fa52d0ab1d493a5d620ff48de3ed9f
Novak <novaldix@gmail.com>
2023-08-10 19:04:54 +0300

    22_6_3 added solution

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 Для достижения хорошей асимптотики решения рекомендуется использовать std::map.
 
 Пример анаграммы: лекарство — стекловар
+
+## <font color="tomato">Полезные ссылки</font>
+
+[How to remove from a map while iterating it? |stackoverflow.com](https://stackoverflow.com/questions/8234779/how-to-remove-from-a-map-while-iterating-it)
+
+[Определить, являются ли две строки анаграммой или нет |techiedelight.com](https://www.techiedelight.com/ru/determine-if-two-strings-are-anagram-or-not/)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <map>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+void eraseEmptyEntriesFromMap(std::map<char, int> &out) {
+    for (auto it = out.begin(), next_it = it; it != out.end(); it = next_it) {
+        ++next_it;
+        if (it->second == 0) out.erase(it);
+    }
+}
+
+bool isAnagramEqual(const string &first, const string &second) {
+    std::map<char, int> frequency;
+
+    for (int i = 0; i < first.length(); ++i) {
+        frequency[first[i]]++;
+        frequency[second[i]]--;
+    }
+    // Удаляем все записи, у которых value == 0
+    eraseEmptyEntriesFromMap(frequency);
+    // Если нет элементов - значит сравниваемые слова одинаковы
+    return frequency.empty();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    string first = "TirstSecondThirdF";
+    string second = "FirstSecondThirdT";
+    cout << std::boolalpha << isAnagramEqual(first, second) << endl;
 }
